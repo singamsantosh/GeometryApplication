172494a2eba0341272f23ddd1cb431e22eb7d059
santosh <singathamizhan.ss@gmail.com>
2014-04-26 15:21:09 +0530

    Added the intro

--- geometry.c
+++ geometry.c
@@ -1,14 +1,17 @@
+
 #include<stdio.h>
 
 int main()
 {
 	// stub for intro message and switch case statements
+        printf("Select the shape");
 	return 0;
 }
 
 int calAreaOfSquare()
 {
     // stub for calculating Area of Square
+    
 }
 int calAreaOfTriangle()
 {
